81397060affc4eb4740b4dfecd90e185403309a2
Vidya Sagar <95843454+ViSaReVe@users.noreply.github.com>
2026-05-04 09:33:58 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,12 @@
+variable = 10         # int type
+
+print(type(variable))
+variable = 98.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello"    # str type
+
+print(type(variable))
+variable = [1, 2, 3]  # list type
+print(type(variable))
