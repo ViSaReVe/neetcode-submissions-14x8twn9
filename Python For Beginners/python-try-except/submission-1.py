9520fad814af95354e022326289e919e2451bdb9
Vidya Sagar <95843454+ViSaReVe@users.noreply.github.com>
2026-06-11 22:05:26 -0700

    Add: python-try-except - submission-1

---
+++ Python For Beginners/python-try-except/submission-1.py
@@ -0,0 +1,7 @@
+def divide_numbers(a: int, b: int) -> None:
+    try:
+        # code that might cause an error
+        result = a / b
+        print(result)
+    except:
+        print("An error occurred!")
