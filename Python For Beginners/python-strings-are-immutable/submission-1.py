ca56faa5c69953f79c52f7f51f98f6bb75040817
Vidya Sagar <95843454+ViSaReVe@users.noreply.github.com>
2026-06-11 17:23:35 -0700

    Add: python-strings-are-immutable - submission-1

---
+++ Python For Beginners/python-strings-are-immutable/submission-1.py
@@ -0,0 +1,10 @@
+def remove_fourth_character(word: str) -> str:
+    pass
+    new_str1 = word[:3]
+    new_str2 = word[4:]
+    return new_str1 + new_str2
+
+
+# do not modify below this line
+print(remove_fourth_character("NeetCode"))
+print(remove_fourth_character("Hello"))
